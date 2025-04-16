f39795e65addd6ed48b34b1d646074678264e76e
dean-mccarthy <deancmccarthy@gmail.com>
2025-04-15 18:38:06 -0700

    initial commit

---
+++ sniped.py
@@ -0,0 +1,22 @@
+import os
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True  # Needed to read messages
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+bot.run('TOKEN')
